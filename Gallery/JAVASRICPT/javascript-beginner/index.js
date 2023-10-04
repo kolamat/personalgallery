e81e684ffc2025ba19eb4af35690b08ad9ce3d03
kolamat <132449653+kolamat@users.noreply.github.com>
2023-10-04 15:29:35 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    Gallery/JAVASRICPT/index.js
    	new file:   Gallery/JAVASRICPT/javascript-beginner/index.js
    	new file:   Gallery/JAVASRICPT/javascript-beginner/indexjs.css
    	renamed:    Gallery/JAVASRICPT/indexjs.html -> Gallery/JAVASRICPT/javascript-beginner/indexjs.html
    	new file:   MO-musics/mo--images/359851819_663110659175964_7495745289972228537_n.jpg
    	new file:   MO-musics/mo--images/383239762_277228008561988_2088531394145955059_n.jpg
    	new file:   MO-musics/mo--images/383401383_7444068968942844_7961307751607708059_n.jpg
    	renamed:    Gallery/JAVASRICPT/indexjs.css -> video-player/app.js
    	new file:   video-player/index.html
    	new file:   video-player/style.css

---
+++ Gallery/JAVASRICPT/javascript-beginner/index.js
@@ -0,0 +1,48 @@
+// document.getElementById("kolade").innerText = 7
+
+// let myAge = 22;
+// let humanDogRatio = 7;
+//
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+//
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + bonusPoints;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// function matanmi() {
+//   console.log("The Button was knacked");
+// }
+//
+// function jingle() {
+//   console.log(42);
+// }
+// jingle();
+//
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+// function lapTimes() {
+//   console.log(lap1 + lap2 + lap3);
+// }
+// lapTimes();
+//
+// let lapsCompleted = 0;
+// function incrementLap() {
+//   lapsCompleted = lapsCompleted + 1;
+// }
+// incrementLap();
+// incrementLap();
+// incrementLap();
+//
+// console.log(lapsCompleted);
+
+let count = 0;
+function matanmi() {
+  console.log("count");
+}
